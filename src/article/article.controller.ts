f1d2f3efc92e1281ec2c737c929ff9689114d90d
Rafael Framil <raframil@gmail.com>
2020-11-16 17:37:04 -0300

    add article controller

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateArticleDto } from './article.dto';
 import { ArticleService } from './article.service';
@@ -18,5 +18,25 @@ export class ArticleController {
     async create(@Body() createArticleDto: CreateArticleDto) {
         const createdArticle = await this.articleService.create(createArticleDto);
         return createdArticle;
+    }
+
+    @Get(':id')
+    async getById(@Param('id') id: string) {
+        return this.articleService.getById(id);
+    }
+
+    @Put(':id')
+    async update(
+        @Param('id') id: string,
+        @Body() article: CreateArticleDto
+    ) {
+        const updatedArticle = await this.articleService.update(id, article);
+        return updatedArticle;
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+        await this.articleService.delete(id);
+        return null;
     }
 }
