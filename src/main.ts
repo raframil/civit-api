e6e9f9ecd6b54ba40f5377e1c403a54639abac72
Rafael Framil <raframil@gmail.com>
2020-11-19 20:34:34 -0300

    add limit to 50mb

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.enableCors();
 
   const options = new DocumentBuilder()
     .setTitle('API Civit')
